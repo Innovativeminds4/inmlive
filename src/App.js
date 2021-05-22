6888da0958e457fbd9254f0f87be3dfca30703f7
ar30612 <ar30612@gmail.com>
2021-05-22 15:52:25 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import "./App.css";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from './components/Home';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path='/' component= { Home } ></Route>
+        <Route  path='/resume' component = { Resume } ></Route>
+        <Route  path='/projects' component = { Projects } ></Route>
+        <Route  path='/contact' component = { Contact } ></Route>
+        <Route component = { Home }></Route>
+      </Switch>
+      <Footer></Footer>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "../App.css";
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+
+  const changeResumeColor = () => {
+    document.getElementById('resume').style.color = 'blue';
+    document.getElementById('projects').style.color = 'grey';
+    document.getElementById('contact').style.color = 'grey';
+  }
+
+  const changeProjectColor = () => {
+    document.getElementById('resume').style.color = 'grey';
+    document.getElementById('projects').style.color = 'blue';
+    document.getElementById('contact').style.color = 'grey';
+  }
+
+  const changeContactColor = () => {
+    document.getElementById('resume').style.color = 'grey';
+    document.getElementById('projects').style.color = 'grey';
+    document.getElementById('contact').style.color = 'blue';
+  }
+
+  const home = () => {
+    document.getElementById('resume').style.color = 'grey';
+    document.getElementById('projects').style.color = 'grey';
+    document.getElementById('contact').style.color = 'grey';
+  }
+
+  return (
+    <div>
+      <div className="row mx-0 my-0 ">
+        <div className="col-7 mx-3 d-flex justify-content-start">
+          <div className="d-inline-block">
+            <div className="roundedCircle1 d-inline-block"></div>
+          </div>
+          <div className="my-2">
+            <NavLink to="/" className="noTextDecorationOnly" onClick={home}>
+              <h4 className="d-inline">Innovative Minds</h4>
+            </NavLink>
+          </div>
+        </div>
+        <div className="links col-3 gy-1">
+          <NavLink
+            to="/resume"
+            id="resume"
+            className="noTextDecoration "
+            activeClassName="active"
+            onClick={changeResumeColor}
+          >
+            Resume
+          </NavLink>
+          <NavLink to="/projects" id="projects" className="noTextDecoration" onClick={changeProjectColor}>
+            Projects
+          </NavLink>
+          <NavLink to="/contact" id="contact" className="noTextDecoration" onClick={changeContactColor}>
+            Contact
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
